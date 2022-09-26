bc0bb6a6576362c2edc08e7799d6f483e55e5c31
samlama111 <sam.horacek@gmail.com>
2022-09-26 11:30:51 +0200

    Creation of Customer model

--- kea_bank/account_management_app/models.py
+++ kea_bank/account_management_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # reached using user.customer https://docs.djangoproject.com/en/4.1/topics/auth/customizing/#extending-the-existing-user-model
+    phone_number = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=40)
+
+    class CustomerRank(models.TextChoices):
+        BASIC='basic'
+        SILVER='silver'
+        GOLD='gold'
+
+    rank = models.CharField(
+        choices=CustomerRank.choices,
+        default=CustomerRank.BASIC
+    )
 
 # Create your models here.
