6f0f52eeaf4fbd5ca9596ef9a64df136cc746477
samlama111 <sam.horacek@gmail.com>
2022-11-07 13:24:58 +0100

    Added default customer to setup script

--- kea_bank/account_management_app/management/commands/setup.py
+++ kea_bank/account_management_app/management/commands/setup.py
@@ -1,7 +1,7 @@
 import secrets
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from account_management_app.models import Account
+from account_management_app.models import Account, Customer
 
 class Command(BaseCommand):
     help = 'Sets up the banks accounts'
@@ -21,3 +21,16 @@ class Command(BaseCommand):
             ops_account.account_number,
             is_loan=True
         )
+
+        default_user = User.objects.create_user('user', email='user@dummy.com', password='test123')
+        default_user.first_name = 'Default'
+        default_user.last_name  = 'User'
+        default_user.save()
+        
+        default_customer = Customer(user=default_user, phone_number='555666')
+        default_customer.save()
+        
+        default_account = Account.objects.create(user=default_user, name='Checking account', is_customer=True)
+        default_account.save()
+
+        
