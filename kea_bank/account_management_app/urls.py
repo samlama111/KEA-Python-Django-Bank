4aaab3cfd7bd13bbd4bc6c84628fd69be77c87c5
samlam111 <sam.horacek@gmail.com>
2023-01-16 16:07:06 +0100

    Fix of URL format

--- kea_bank/account_management_app/urls.py
+++ kea_bank/account_management_app/urls.py
@@ -11,14 +11,14 @@ urlpatterns = [
     path('my-profile/', views.my_profile, name="my_profile"),
     path('chatbot/', views.chatbot_messages, name="chatbot_messages"),
     path('message/', views.chatbot_conversation, name = "chatbot_conversation" ),
-    path('account_details/<int:account_number>/', views.account_details, name="account_details"),
+    path('account-details/<int:account_number>/', views.account_details, name="account_details"),
     path('loan/<int:account_number>/', views.loan, name="loan"),
     path('loan/<int:account_number>/pay/', views.pay_back_loan, name="pay_back_loan"),
-    path('my_savings/', views.my_savings, name="my_savings"),
+    path('my-savings/', views.my_savings, name="my_savings"),
     path('create-saving-account/', views.create_saving_account, name="create_saving_account"),
     path('saving-account-detail/<int:account_number>/', views.saving_account_detail, name="saving_account_detail"),
     path('delete-account/<int:account_number>', views.delete_saving_account, name="delete_saving_account"),
-    path('saving_account_transfer/<int:account_number>/', views.saving_account_transfer, name="saving_account_transfer"),
+    path('saving-account-transfer/<int:account_number>/', views.saving_account_transfer, name="saving_account_transfer"),
     path('api/v1/transaction', Reserve.as_view()), #post
     path('api/v1/transaction/<uuid:token>/', GetUpdateStatus.as_view()), #get, put
 ]
