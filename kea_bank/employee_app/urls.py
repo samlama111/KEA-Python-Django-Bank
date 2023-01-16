2f1439a7e19a24c41f5486b9ba65b09b4485f0c3
samlam111 <sam.horacek@gmail.com>
2023-01-16 16:11:41 +0100

    Removed extra imports

--- kea_bank/employee_app/urls.py
+++ kea_bank/employee_app/urls.py
@@ -9,9 +9,7 @@ urlpatterns = [
     path('update-customer/<int:pk>/', views.update_customer, name="update_customer"),
     path('my-profile/', views.my_profile, name="my_profile"),
     path('create-customer/', views.create_customer, name="create_customer"),
-    path('account_detail/<int:account_number>/', views.account_detail, name="account_detail"),
+    path('account-detail/<int:account_number>/', views.account_detail, name="account_detail"),
     path('create-account/<int:pk>', views.create_account, name="create_account"),
     path('delete-account/<int:account_number>/<int:pk>', views.delete_account, name="delete_account")
-
-
 ]
